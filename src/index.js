953a16b8ded68b65dd2fbd25739f93eabc39df65
AhsanAzim <ahsan-azim@live.com>
2016-07-12 16:38:30 -0400

    basic reqs complete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-// change require to es6 import style
-import $ from 'jquery';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import './style.scss';
+import debounce from 'lodash.debounce';
 
-// counter for number of seconds elapsed
-let num = 1;
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-function update() {
-  $('#main').html(`You\'ve been on this page for ${num} seconds!`);
-  num += 1;
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    this.search('pixar');
+    this.search = debounce(this.search, 300);
+  }
+
+  search(text) {
+    youtubeSearch(text).then(videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchChange={text => this.search(text)} />
+        <div id="video-section">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
+        </div>
+      </div>
+    );
+  }
 }
 
-// updates text of #main every second
-setInterval(update, 1000);
+ReactDOM.render(<App />, document.getElementById('main'));
